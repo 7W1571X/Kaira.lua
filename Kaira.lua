4f9025f61ec5d6c8d723aa9414a659e7adce810a
Twisted <7w1571x@gmail.com>
2023-03-06 09:32:31 -0600

    Start of Kaira

---
+++ Kaira.lua
@@ -0,0 +1,14 @@
+local Kaira = {
+    _VERSION       = 'Kaira.lua 0.0.1',
+    _LINK          = 'N/A',
+    _DESCRIPTION   = 'Muilti QOL Lua for https://legions.win/.',
+    _LICENSE       = [[
+        Kaira (c) by Twis7ed
+
+        Kaira is licensed under a
+        Creative Commons Attribution 1.0 Universal.
+
+        You should have received a copy of the license along with this
+        work. If not, see http://creativecommons.org/licenses/by/1.0/.
+    ]]
+}
